9f25684a8684d83e411a5c062149add6fdc8f1da
nafisahinaf <nafisahkarimah@mail.ugm.ac.id>
2019-12-08 01:28:46 +0700

    first commit

---
+++ database/factories/ArtikelFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\artikel;
+use Faker\Generator as Faker;
+
+$factory->define(artikel::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
